d85e9df7e4f289532586878bde1b23804046b226
github-cloudlabsuser-1233 <167997823+github-cloudlabsuser-1233@users.noreply.github.com>
2024-11-28 15:34:27 +0000

    Copilot third commit

---
+++ powershell.ps1
@@ -0,0 +1,21 @@
+# Variables
+$resourceGroupName = "example-resources"
+$location = "West Europe"
+$storageAccountName = "examplestorageacct"
+
+# Login to Azure
+Connect-AzAccount
+
+# Create Resource Group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create Storage Account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+                     -Name $storageAccountName `
+                     -Location $location `
+                     -SkuName "Standard_LRS" `
+                     -Kind "StorageV2"
+
+# Output Storage Account Primary Endpoint
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$storageAccount.PrimaryEndpoints.Blob
